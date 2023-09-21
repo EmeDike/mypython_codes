f0e338ce33b2b562e3236a953d524c7e1f95628a
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-09-21 09:38:02 +0100

    Update assignment_inventory_management.py

--- assignment_inventory_management.py
+++ assignment_inventory_management.py
@@ -9,6 +9,8 @@ while True:
     print("3. Display Balance")
     print("4. Exit")
 
+    # use one print statement
+
     choice = int(input("Enter your choice: "))
     if choice == 1:
         quantity = int(input("Enter received quantity "))
@@ -22,6 +24,9 @@ while True:
         quantity_dispensed = int(input("Enter quatity of Stock dispensed"))
         balance_after_inflow -= quantity_dispensed
         print(f"ITEMS SUCCESSFULLY DELIVERED")
+
+# refactr this code.
+
 
 
 
